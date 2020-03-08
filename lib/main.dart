6ecc003b7a04a2cfeee61e84cbcad90372c1006f
Matthew Gleich <matthewgleich@gmail.com>
2020-03-07 23:51:36 -0400

    Update more app fonts

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,23 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: "Raleway",
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontFamily: "RobotoCondensed",
+              ),
+            ),
       ),
       home: CategoriesScreen(),
     );
