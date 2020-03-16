5daa0a4464942cefef8f8a331a40058eb5daeda3
Matthew Gleich <matthewgleich@gmail.com>
2020-03-15 23:59:11 -0400

    Update application architecture

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './../dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static const screenName = "/category-meals";
   // final String categoryId;
@@ -13,12 +15,18 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs["title"];
     final categoryID = routeArgs["id"];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryID))
+        .toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
       body: ListView.builder(
-        itemBuilder: (ctx, index) {},
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:meal_app/category_meals_screen.dart';
+import './../screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
