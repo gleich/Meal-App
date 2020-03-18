c5b3877f6e5696bfb5efae9310a4be0379a7a204
Matthew Gleich <matthewgleich@gmail.com>
2020-03-17 23:40:45 -0400

    Implement remove temp meal functionality

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -52,14 +52,15 @@ class MainDrawer extends StatelessWidget {
             "Meals",
             Icons.restaurant,
             () {
-              Navigator.of(context).pushNamed("/");
+              Navigator.of(context).pushReplacementNamed("/");
             },
           ),
           _buildListTile(
             "Filters",
             Icons.settings,
             () {
-              Navigator.of(context).pushNamed(FilterScreen.routeName);
+              Navigator.of(context)
+                  .pushReplacementNamed(FilterScreen.routeName);
             },
           ),
         ],
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -10,6 +10,7 @@ class MealItem extends StatelessWidget {
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
+  final Function removeItem;
 
   MealItem({
     @required this.id,
@@ -18,6 +19,7 @@ class MealItem extends StatelessWidget {
     @required this.complexity,
     @required this.duration,
     @required this.affordability,
+    @required this.removeItem,
   });
 
   String get complexityText {
@@ -47,9 +49,17 @@ class MealItem extends StatelessWidget {
   }
 
   void _selectMeal(BuildContext context) {
-    Navigator.of(context).pushNamed(
+    Navigator.of(context)
+        .pushNamed(
       MealDetailScreen.routeName,
       arguments: id,
+    )
+        .then(
+      (result) {
+        if (result != null) {
+          removeItem(result);
+        }
+      },
     );
   }
 
